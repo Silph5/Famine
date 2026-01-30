4d5d5955349f28a2e774bb071c50bb92a78c412d
Silph5 <tj.adamson@sky.com>
2026-01-30 22:29:11 +0000

    added /adminhelp to /help for visibility purposes

--- src/bot.py
+++ src/bot.py
@@ -379,6 +379,7 @@ async def cmdHelp(interaction:discord.Interaction):
     helpEmbed.add_field(name="/winstats", value="Shows your highest win achievement in every role, alongside a minimum estimate of your total wins. Requires a linked account.\n\n-# Usage example: `/winstats`", inline=False)
     helpEmbed.add_field(name="/nextunobtained", value="Shows the next most common achievement that you have not completed. Requires a linked account.\n\n-# Usage example: `/nextunobtained`", inline=False)
     helpEmbed.add_field(name="/getrolefactioncode", value="Gives a code that can you typed or pasted into game chat which produces a role mention of a given role as being aligned with a given faction. For example, a coven-aligned executioner \n\n-# Usage example: `/getrolefactioncode cs Town`", inline=False)
+    helpEmbed.add_field(name="/adminhelp", value="Presents a list of per-guild bot configuration commands. This command can only be used by server administrators. \n\n-# Usage example: `/adminhelp`", inline=False)
 
 
     await interaction.response.send_message(embed=helpEmbed)
